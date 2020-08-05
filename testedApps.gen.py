0ae1bca5c20d92fa35da14b51d40ad232f3c71ab
Dmitriy Vornychev <dm.s.vornychev@gmail.com>
2020-08-05 13:23:13 +0300

    Print path to tested app in doc comment

--- testedApps.gen.py
+++ testedApps.gen.py
@@ -1,5 +1,6 @@
 """Auto-generate lib/testedApps.d.ts from TestComplete's config"""
 
+from os import path
 from sys import argv
 from typing import TextIO
 from xml.etree import ElementTree
@@ -10,7 +11,7 @@ def printHeader(destinationFile: TextIO, baseFile: str):
     destinationFile.write(f'''/**
  * Declaration of all tested apps.
  * NOTE: Auto-generated from {baseFile}
- */\n
+ */
 ''')
 
 
@@ -18,6 +19,18 @@ def parseXML(xmlFile: str, destinationFile: TextIO):
     for children in ElementTree.parse(xmlFile).getroot().findall('children'):
         for child in children.findall('child'):
             for config in child.findall('config'):
+                runModeParams = config.find('runModeParams')
+                if runModeParams:
+                    activeMode = runModeParams.attrib['activeRunMode']
+                    for param in runModeParams.findall('param'):
+                        if param.attrib['name'] == activeMode:
+                            doc = f'    /** {path.abspath(param.attrib["filePath"]) + path.sep}{param.attrib["fileName"]}'
+                            params = param.attrib["parameters"]
+                            if params:
+                                doc += ' ' + params
+                            doc += ' */\n'
+                            destinationFile.write(doc)
+
                 destinationFile.write(
                     f'    const {config.attrib["nodeName"]}: TestComplete.TestedApp;\n')
 
@@ -32,7 +45,9 @@ def generateFile(sourceXmlPath: str, destinationDTs: str):
 
 if __name__ == "__main__":
     if len(argv) < 3:
-        print(f"Usage: {__file__} <path to TestedApps.tcTAs> <path to output testedApps.d.ts>")
+        print(
+            f"Usage: {__file__} <path to TestedApps.tcTAs> <path to output testedApps.d.ts>"
+        )
         exit(1)
 
     print(f"\n- Use {argv[1]} to generate {argv[2]}\n")
